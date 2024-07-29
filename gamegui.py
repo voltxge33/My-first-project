03d9de4bb08b730718c23dedd94e123fb2b77e28
voltxge33 <theleonardfamilycalendar@gmail.com>
2024-07-29 15:05:37 -0400

    Added more exception handling and fixed other stuff as well

--- gamegui.py
+++ gamegui.py
@@ -14,15 +14,15 @@ def restart_script():
 
 
 def second_window():
+
     # Window for guessing
-    
     window2 = tk.Toplevel(window1)
     window2.title("Guess!")
     window2.geometry("350x150")
     window2.configure(bg="black")
     # Title
 
-    title_label = ttk.Label(text="Number Guessing Game", master=window2, font ="Calibri 15 bold")
+    title_label = ttk.Label(text="Number Guessing Game", master=window2, font ="Calibri 12 bold")
     title_label.configure(background="black", foreground="white")
     title_label.pack()
 
@@ -53,22 +53,30 @@ def second_window():
     output_label.pack()
 
 
+def random_num():
+    global random_number
+    random_number = random.randint(input1.get(), input2.get())
+    print(random_number)  # Add this line to check the value
 
 
 def guess(react):
     global guesses
     react.set("You got this!")
-    if answer.get() == random_num:
-        react.set("You did it! Nice! Would you like to play again?")
+
+    try:
+        user_answer = int(answer.get())
+    except ValueError:
+        react.set("Please enter a valid number.")
+        return
+    if user_answer == random_number:
+        react.set("Good Job! Would you lke to restart?")
         button3.pack(pady=5)
-    elif guesses == 0:
-        react.set("Out of guesses, would you like to try again?")
+    if guesses == 0:
+        react.set("Out of guesses, would you like to try again? The answer was " + str(random_number))
         button3.pack(pady=5)
-    elif answer.get() != random_num and answer.get().isnumeric():
-        react.set("Incorrect! You have " + str(guesses) + " left")
+    elif user_answer != random_number:
+        react.set("Incorrect! You have " + str(guesses) + " guesses left")
         guesses -= 1
-    elif answer.get().isalpha():
-        react.set("That's not right, pick a number next time please.")
 
 
 # window for choosing the range of numbers
@@ -78,7 +86,7 @@ window1.geometry("350x150")
 window1.configure(bg="black")
 # Title
 
-title_label1 = tk.Label(text="Number Guessing Game", master=window1, font = "Calibri 15 bold")
+title_label1 = tk.Label(text="Number Guessing Game", master=window1, font = "Calibri 12 bold")
 title_label1.configure(background="black", foreground="white")
 title_label1.pack()
 
@@ -90,11 +98,11 @@ input2 = tk.IntVar()
 input_frame1 = tk.Frame(master=window1, bg='black')
 entry_field1 = tk.Entry(master = input_frame1, textvariable=input1)
 entry_field2 = tk.Entry(master=input_frame1, textvariable=input2)
-button1 = tk.Button(master = input_frame1, text="Let's go!", command=second_window)
+button1 = tk.Button(master = input_frame1, text="Let's go!", command=lambda: [second_window(), random_num()])
 entry_field1.pack(side="left", padx = 5)
 entry_field2.pack(side="left")
 button1.pack(side="left", padx=5)
 input_frame1.pack()
-random_num = random.randint(input1.get(), input2.get())
+
 # run
 window1.mainloop()
